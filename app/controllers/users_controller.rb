34c92e72d9e36d3bda82345d4d3d643fce95a725
katsura46 <katsura2106@outlook.jp>
2022-05-02 14:05:31 +0000

    [Update]5-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,19 +28,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def followings
-    @followings = @user.following_users
-  end
-
-  def followers
-    @followers = @user.follower_users
-  end
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
