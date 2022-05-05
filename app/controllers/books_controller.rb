f0a1c74003da9fd780c5970faccd14e5c5073e77
katsura46 <katsura2106@outlook.jp>
2022-05-05 14:55:41 +0000

    [Start]5-6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,13 +18,13 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
 
-      @books = Book.all
       render 'index'
     end
   end
