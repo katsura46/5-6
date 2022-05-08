6b193302e9931fce87c29f954784551a1fbfe5da
katsura46 <katsura2106@outlook.jp>
2022-05-08 08:59:27 +0000

    [Finish]5-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,20 +5,17 @@ class BookCommentsController < ApplicationController
     @comment = BookComment.new(comment_params)
     @comment.user_id = current_user.id
     @comment.book_id = @book.id
-    if @comment.save
-      render :book_comments/book_comments
-    else
-      @book_new = Book.new
-      @comments = @book.comments
-      redirect_to new_post_path
-    end
+    @comment.save
+
+
+
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @comment = BookComment.find(params[:id])
     @comment.destroy
-    render :book_comments/book_comments
+
   end
 
   private
